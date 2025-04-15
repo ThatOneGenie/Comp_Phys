81aacc3d5cd9341bc60308dcfa96a76d50818324
Preston Seligman <golfpresto@gmail.com>
2025-04-15 01:32:05 -0500

    Homeworks Added

---
+++ Final/README.md
@@ -0,0 +1,3 @@
+<h1>Final Project</h1>
+
+Add something here.
---
+++ HW6/rk4.py
@@ -0,0 +1,55 @@
+""" From "COMPUTATIONAL PHYSICS", 3rd Ed, Enlarged Python eTextBook  
+    by RH Landau, MJ Paez, and CC Bordeianu
+    Copyright Wiley-VCH Verlag GmbH & Co. KGaA, Berlin;  Copyright R Landau,
+    Oregon State Unv, MJ Paez, Univ Antioquia, C Bordeianu, Univ Bucharest, 2015.
+    Support by National Science Foundation
+    
+    Simplified and adapted by Lev Kaplan 2019"""
+
+# rk4.py 4th order Runge Kutta
+
+
+from pylab import *		 
+
+ydumb = zeros(2);       y = zeros(2)
+fReturn = zeros(2);     k1 = zeros(2, float)
+k2 = zeros(2, float);   k3 = zeros(2, float) 
+k4 = zeros(2, float)
+
+def f( t, y):      # Force function: component 0 is position, component 1 is velocity
+    fReturn[0] = y[1]       # dx/dt  = v                                     
+    fReturn[1] = -y[0]      # dv/dt = -x   
+    return fReturn
+
+
+def rk4(t,h,n):          # take one 4th order RK step          
+                          # evolve y from time t to time t+h
+                          # n is number of variables to evolve
+                          # k1,k2,k3,k4 are four estimates for Delta y
+    k1 = h*f(t, y)       # note that we use vector (array) notation instead of using loops
+                         # see text for how to do this the long way with loops
+    ydumb = y + k1/2     #estimate for midpoint using Euler
+    k2 = h*f(t+h/2, ydumb) 
+    ydumb = y + k2/2     #another estimate for midpoint
+    k3 = h*f(t+h/2, ydumb)
+    ydumb = y + k3       #estimate for ending point of interval
+    k4 = h*f(t+h, ydumb) 
+    ynew = y + (k1 + 2*(k2 + k3) + k4)/6
+    return ynew   
+
+a = 0.     # evolve from time a to time b in n steps
+b = 10.
+n = 100
+t = a;       h = (b-a)/n;
+
+y[0] = 1;   y[1] = 0    #initialize position and velocity
+ 
+
+while (t < b):                                              # Time loop
+    if ((t + h) > b):
+        h = b - t                                           # Last step
+    y = rk4(t,h,2)
+    t = t + h
+    
+print(y)   #print out final position and velocity
+                                
